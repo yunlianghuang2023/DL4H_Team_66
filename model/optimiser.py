bfbc771de6107bb4107101f09577e033bf3b2ac2
yunlianghuang2023 <139084839+yunlianghuang2023@users.noreply.github.com>
2024-04-14 20:31:13 -0700

    Update optimiser.py

--- model/optimiser.py
+++ model/optimiser.py
@@ -1,4 +1,4 @@
-import pytorch_pretrained_bert as Bert
+from transformers import AdamW
 
 def adam(params, config=None):
     if config is None:
@@ -10,11 +10,9 @@ def adam(params, config=None):
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
 
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in params if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-        {'params': [p for n, p in params if any(nd in n for nd in no_decay)], 'weight_decay': 0}
+        {'params': [p for n, p in params if not any(nd in n for nd in no_decay)], 'weight_decay': config['weight_decay']},
+        {'params': [p for n, p in params if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
 
-    optim = Bert.optimization.BertAdam(optimizer_grouped_parameters,
-                                       lr=config['lr'],
-                                       warmup=config['warmup_proportion'])
+    optim = AdamW(optimizer_grouped_parameters, lr=config['lr'], betas=(0.9, 0.999), eps=1e-8, correct_bias=True)
     return optim
